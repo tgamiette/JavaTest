af143e4e06b15be25d4f819d58b62cf0be1e4fd2
Teddy Gamiette <tgamiette@kernix.com>
2024-04-09 14:46:08 +0200

    init

--- src/fr/hetic/Calculateur.java
+++ src/fr/hetic/Calculateur.java
@@ -3,19 +3,18 @@ package fr.hetic;
 public class Calculateur {
     public static void main(String[] args) {
 
-        Double num1 ;
-        Double num2 ;
+        Double num1;
+        Double num2;
+
         try {
-              num1 = Double.parseDouble(args[0]);
-              num2 = Double.parseDouble(args[1]);
+            num1 = Double.parseDouble(args[0]);
+            num2 = Double.parseDouble(args[1]);
         } catch (NumberFormatException e) {
             System.out.println("Les deux premiers arguments doivent être des nombres");
             return;
         }
-//
         String operator = args[2];
-//
-        Double resultat ;
+        Double resultat;
 
         switch (operator) {
             case "+":
